ea94be517986333e4755b2c68f9802e72f3bac54
szczepaniakmichal <mssk87@gmail.com>
2020-08-16 22:15:13 +0200

    js - get fields / add 'click' to change class and player, set game values /

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
+const gameItems = [...document.querySelectorAll('.game__item')];
+const gameValues = ['', '', '', '', '', '', '', '', ''];
+let activePlayer = 'x';
+
+gameItems.forEach(field => {
+    field.addEventListener('click', e => {
+        const {position} = e.target.dataset;
+        if (gameValues[position] === '') {
+            gameValues[position] = activePlayer;
+            e.target.classList.add(`game__item--${activePlayer}`)
+            activePlayer = activePlayer === 'x' ? 'o' : 'x';
+        }
+    })
+})
+
 
